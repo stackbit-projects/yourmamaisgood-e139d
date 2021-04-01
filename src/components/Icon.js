acf79d70c3e0badf78f1ba66044b76b4aad72efd
Stackbit Code Editor <projects@stackbit.com>
2021-04-01 13:57:49 +0000

    Icon.js: changed

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -12,6 +12,7 @@ export default class Icon extends React.Component {
             	<path d="M12 2l-1.762 1.763 6.975 6.988h-15.212v2.5h15.212l-6.975 6.988 1.763 1.762 10-10z" />
                 ) : ((icon === 'globe') ? (
 
+                    
                     ) : ((icon === 'phone') ? (
                 <path d="M58.9 47l-10.4-6.8a4.8 4.8 0 0 0-6.5 1.3c-2.4 2.9-5.3 7.7-16.2-3.2S19.6 24.4 22.5 22a4.8 4.8 0 0 0 1.3-6.5L17 5.1c-.9-1.3-2.1-3.4-4.9-3S2 6.6 2 15.6s7.1 20 16.8 29.7S39.5 62 48.4 62s13.2-8 13.5-10-1.7-4.1-3-5z" fill="none" stroke="#202020" stroke-miterlimit="10" stroke-width="2" stroke-linejoin="round" stroke-linecap="round"></path>   
                 ) : ((icon === 'close') ? (
